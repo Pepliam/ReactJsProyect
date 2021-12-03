19480924c737d81d4fce09bad51f5d9be07ef8f1
Pepliam <julyxene@hotmail.com>
2021-12-03 00:55:21 -0300

    pongo barra nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function NavBar() {
+    return ( 
+        <nav class = "navbar navbar-expand-sm bg-light">
+            <ul class = "navbar-nav">
+                <li class = "nav-item">
+                    <a class = "nav-link"> Link 1 </a></li> 
+                <li class = "nav-item">
+                    <a class = "nav-link"> Link 2 </a></li>
+                <li class = "nav-item">
+                    <a class = "nav-link"> Link 3 </a></li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default NavBar;
